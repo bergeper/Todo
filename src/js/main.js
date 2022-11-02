7cd3efc884493b181d69d4ce8733135ab2786a9e
Per Berge <berge.per2493@gmail.com>
2022-11-02 20:23:48 +0100

    I got stuck and had to rethink, added a loop inside a function instead.

--- src/js/main.js
+++ src/js/main.js
@@ -1,71 +1,66 @@
 import { Todo } from "./todo";
 
-// List with Incomplete Objects
-let todoList = [
+// creating objects
+let todos = [
   new Todo("Handla Mat", false),
   new Todo("Springa/Träna", false),
   new Todo("Uppdatera din portfolio", false),
   new Todo("Laga matlådor", false),
 ];
+// pushing the new objects into a list
+let todoList = [];
+for (let i = 0; i < todos.length; i++) {
+  todoList.push(todos[i]);
+}
 
 // Getting the article tag.
-let todoListIncomplete = document.getElementById("todoListIncomplete");
-// Add objects done to new list???
-//let todoListComplete = document.getElementById("todoListComplete");
-
+const todoListContainer = document.getElementById("todoListDisplay");
 // input and button
-document.getElementById("addTask").addEventListener("click", () => {
-  createTodo(document.getElementById("task").value);
-});
+const inputTask = document.getElementById("task");
+const buttonTask = document.getElementById("addTask");
 
+displayTodoList();
+
+buttonTask.addEventListener("click", createTodo);
 // Create a todo
-function createTodo(todoNew, completedNew) {
-  let newTodo = new Todo(todoNew, (completedNew = false));
+function createTodo() {
+  const newTodo = new Todo(inputTask.value, false);
   todoList.push(newTodo);
-  addTodo(newTodo);
+
+  inputTask.value = "";
+  displayTodoList(newTodo);
 }
 
 // Adding objects to the list
-function addTodo(todo) {
-  const listItem = document.createElement("p");
-  listItem.classList.add("content__list--item");
-  if (todo.completed === false) {
-    todoListIncomplete.appendChild(listItem);
+function displayTodoList() {
+  // clear when adding new item to list
+  todoListContainer.innerHTML = "";
+  for (let i = 0; i < todoList.length; i++) {
+    // create elements too display the list.
+    const listItemContainer = document.createElement("div");
+    listItemContainer.classList.add("card__task");
+    // task-checked
+    const listItemChecked = document.createElement("input");
+    listItemChecked.classList.add("card__task--check");
+    listItemChecked.type = "checkbox";
+    // task-name
+    const listItemTask = document.createElement("p");
+    listItemTask.classList.add("card__task--name");
+    // task-done
+    const listItemRemove = document.createElement("span");
+    listItemRemove.classList.add("card__task--remove");
+
+    // what each element should display.
+    listItemRemove.innerHTML = `<i class="bi bi-x-lg"></i>`;
+    listItemTask.innerText += todoList[i].task;
+
+    // appended all the elements in the right order.
+    listItemContainer.appendChild(listItemChecked);
+    listItemContainer.appendChild(listItemTask);
+    listItemContainer.appendChild(listItemRemove);
+    todoListContainer.appendChild(listItemContainer);
   }
-  listItem.innerText = todo.task;
+
   // just to see what happends
   console.log(todoList);
 }
-
-function doneTodo(todo) {
-  for (let i = 0; i < todoList.length; i++) {
-    if (todo[i].completed === true) {
-      console.log("klar");
-    } else {
-      console.log("inte klar");
-    }
-  }
-}
-
-// Loops the list
-for (let i = 0; i < todoList.length; i++) {
-  const listItem = document.createElement("p");
-  listItem.addEventListener(() => {
-    doneTodo(todo);
-  });
-  listItem.classList.add("content__list--item");
-  listItem.innerText += todoList[i].task;
-  todoListIncomplete.appendChild(listItem);
-}
-
-done.addEventListener("click", () => {
-  if (done.checked === true) {
-    text.classList.add("done");
-
-    console.log("du kryssade i done");
-  } else {
-    text.classList.remove("done");
-  }
-});
-// Function to mark object done.
-// veta vad som är klart.
