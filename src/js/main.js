3123b7c2172b3755e850d02f3646fd1306ea6319
Per Berge <berge.per2493@gmail.com>
2022-11-02 12:15:31 +0100

    added functions for adding todos and looping the list

--- src/js/main.js
+++ src/js/main.js
@@ -2,19 +2,43 @@ import { Todo } from "./todo";
 
 // List with Incomplete Objects
 let todoList = [
-  new Todo(1, "Handla Mat", false),
-  new Todo(2, "Springa/Träna", false),
-  new Todo(3, "Uppdatera din portfolio", false),
-  new Todo(4, "Laga matlådor", false),
+  new Todo("Handla Mat", false),
+  new Todo("Springa/Träna", false),
+  new Todo("Uppdatera din portfolio", false),
+  new Todo("Laga matlådor", false),
 ];
 
 // List with Completed objects
-let todoListDone = [];
+let todoListIncomplete = document.getElementById("todoListIncomplete");
+let todoListComplete = document.getElementById("todoListComplete");
 
-todoListIncomplete = document.getElementById("todoListIncomplete");
-todoListComplete = document.getElementById("todoListComplete");
-document.getElementById("task").value;
+// input and button
+document.getElementById("addTask").addEventListener("click", () => {
+  createTodo(document.getElementById("task").value);
+});
+// Create a todo
+function createTodo(todo, completed) {
+  let newTodo = new Todo(todo, (completed = false));
+  todoList.push(newTodo);
+  addTodo(newTodo);
+}
 
+// Adding objects to the list
+function addTodo(todo) {
+  const listItem = document.createElement("p");
+  listItem.classList.add("content__list--item");
+
+  if (todo.completed === false) {
+    todoListIncomplete.appendChild(listItem);
+  } else {
+    todoListComplete.appendChild(listItem);
+  }
+  listItem.innerText = todo.task;
+  // just to see what happends
+  console.log(todoList);
+}
+
+// loops the list
 for (let i = 0; i < todoList.length; i++) {
   const listItem = document.createElement("p");
 
--- src/js/todo.js
+++ src/js/todo.js
@@ -1,7 +1,6 @@
 // class for each object in list
 export class Todo {
   constructor(task, completed) {
-    this.id = id;
     this.task = task;
     this.completed = completed;
   }
