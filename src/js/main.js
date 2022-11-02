3d299278bbeb0713746b3c1b7eaa79c5cf62c5bb
Per Berge <berge.per2493@gmail.com>
2022-11-02 13:27:17 +0100

    Working on to mark objects as done

--- src/js/main.js
+++ src/js/main.js
@@ -8,17 +8,19 @@ let todoList = [
   new Todo("Laga matlådor", false),
 ];
 
-// List with Completed objects
+// Getting the article tag.
 let todoListIncomplete = document.getElementById("todoListIncomplete");
-let todoListComplete = document.getElementById("todoListComplete");
+// Add objects done to new list???
+//let todoListComplete = document.getElementById("todoListComplete");
 
 // input and button
 document.getElementById("addTask").addEventListener("click", () => {
   createTodo(document.getElementById("task").value);
 });
+
 // Create a todo
-function createTodo(todo, completed) {
-  let newTodo = new Todo(todo, (completed = false));
+function createTodo(todoNew, completedNew) {
+  let newTodo = new Todo(todoNew, (completedNew = false));
   todoList.push(newTodo);
   addTodo(newTodo);
 }
@@ -27,23 +29,43 @@ function createTodo(todo, completed) {
 function addTodo(todo) {
   const listItem = document.createElement("p");
   listItem.classList.add("content__list--item");
-
   if (todo.completed === false) {
     todoListIncomplete.appendChild(listItem);
-  } else {
-    todoListComplete.appendChild(listItem);
   }
   listItem.innerText = todo.task;
   // just to see what happends
   console.log(todoList);
 }
 
-// loops the list
+function doneTodo(todo) {
+  for (let i = 0; i < todoList.length; i++) {
+    if (todo[i].completed === true) {
+      console.log("klar");
+    } else {
+      console.log("inte klar");
+    }
+  }
+}
+
+// Loops the list
 for (let i = 0; i < todoList.length; i++) {
   const listItem = document.createElement("p");
-
+  listItem.addEventListener(() => {
+    doneTodo(todo);
+  });
   listItem.classList.add("content__list--item");
   listItem.innerText += todoList[i].task;
-
   todoListIncomplete.appendChild(listItem);
 }
+
+done.addEventListener("click", () => {
+  if (done.checked === true) {
+    text.classList.add("done");
+
+    console.log("du kryssade i done");
+  } else {
+    text.classList.remove("done");
+  }
+});
+// Function to mark object done.
+// veta vad som är klart.
